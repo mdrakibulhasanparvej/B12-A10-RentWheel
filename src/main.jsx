f9cd31960e0c81661c32c175d26aba7e2ae5d088
unknown <mdrakibulhasanparvej>
2025-11-10 01:13:38 +0600

    firebase implementation

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/Router";
+import AuthProvider from "./proviedrs/AuthProvider";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+      <ToastContainer />
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/proviedrs/AuthProvider.jsx
+++ src/proviedrs/AuthProvider.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useState } from "react";
+import auth from "../firebase/firebase.config";
 
-const AuthProvider = () => {
-    return (
-        <div>
-            <h2></h2>
-        </div>
-    );
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+
+  //creatign user for with pass email;
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authData = {
+    createUser,
+    loading,
+    setLoading,
+  };
+
+  return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
